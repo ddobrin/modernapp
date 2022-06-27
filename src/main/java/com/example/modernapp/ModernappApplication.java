e18b37fa0f9bc7cc97dc3c98a7a3bb166ac04c9f
ddobrin <ddobrin@google.com>
2022-06-27 16:30:33 -0400

    in sync with template

---
+++ src/main/java/com/example/modernapp/ModernappApplication.java
@@ -0,0 +1,13 @@
+package com.example.modernapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModernappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModernappApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/modernapp/actuator/StartupCheck.java
@@ -0,0 +1,39 @@
+package com.example.modernapp.actuator;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.stereotype.Component;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Component
+@Endpoint(id="startup")
+public class StartupCheck {
+    // logger
+    private static final Log logger = LogFactory.getLog(StartupCheck.class);
+
+    private static boolean status = false;
+
+    public static void up(){ status = true;}
+    public static void down(){ status = false;}
+
+    @ReadOperation
+    public CustomData customEndpoint() {
+        Map<String, Object> details = new LinkedHashMap<>();
+        if (!status) {
+            logger.info("modernapp Startup Endpoint: Application is ready to serve traffic !");
+            return null;
+        }
+
+        logger.info("modernapp Startup Endpoint: Application is ready to serve traffic !");
+
+        CustomData data = new CustomData();
+        details.put("StartupEndpoint", "modernapp Startup Endpoint: Application is ready to serve traffic");
+        data.setData(details);
+
+        return data;
+    }
+}
